120dd36952faa456595ca26abe386ff318640f0e
Mukul Aryal <mukul.development@gmail.com>
2023-04-08 10:44:00 +0545

    Added comments

--- utilities/resize_images.py
+++ utilities/resize_images.py
@@ -1,16 +1,18 @@
-import PIL
 import os
 from PIL import Image
 
-f = r"D:/Annotated_Data/training_images_1"
+# Script to resize all images to 224x224 pixels
+
+f = r"D:/Annotated_Data/training_images_1" # Path to directory
 
 os.listdir(f)
 
+# Loop through all files in the directory
 for file in os.listdir(f):
-    f_img = f+"/"+file
+    f_img = f+"/"+file # Store image data i variable
     img = Image.open(f_img)
-    img = img.resize((224,224))
-    print(f"Resizing {file}....")
+    img = img.resize((224,224)) 
+    print(f"Resizing {file}....") # Print to keep track of progress
     img.save(f_img)
 
-print("Sakkyo")
+print("All Done!")
